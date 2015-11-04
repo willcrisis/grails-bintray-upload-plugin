547ba84e576fcc2fb0743cafe23efcb1b8adc777
Willian Krause <krause.willian@gmail.com>
2015-11-04 17:32:08 -0200

    Cleanup

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,12 +1,9 @@
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir = "target/test-reports"
-grails.project.target.level = 1.7
-grails.project.source.level = 1.7
 grails.project.repos.bintray.url = "https://api.bintray.com/maven/willcrisis/plugins/grails-bintray-upload"
 grails.project.repos.bintray.username = System.getenv("BINTRAY_USERNAME")
 grails.project.repos.bintray.password = System.getenv("BINTRAY_PASSWORD")
 grails.project.repos.default = "bintray"
+
+grails.project.work.dir = 'target'
 
 grails.project.dependency.resolver = "maven"
 grails.project.dependency.resolution = {
